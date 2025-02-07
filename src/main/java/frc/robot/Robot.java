c8721120a90e438bfee18b219179d2377a484cf7
maxwell.lee.372007 <maxwell.lee.372007@gmail.com>
2025-02-07 07:51:43 -0500

    shuffleboard voltage and maxspeed; logging cleanup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,14 +11,17 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 
 public class Robot extends TimedRobot {
-  private static final int kMotorPort = 0;
+  private static final int kMotorPort = 11;
 
 
   private final TalonFX m_motor;
   private boolean m_toggle = true;
+  private double voltageOut = 2; // Volts
+  private double maxSpeed = 2000; // RPM
 
   public Robot() {
     m_motor = new TalonFX(kMotorPort);
@@ -32,7 +35,6 @@ public class Robot extends TimedRobot {
     m_motor.setNeutralMode(NeutralModeValue.Coast);
 
     SignalLogger.setPath("/media/sda11/");
-    SignalLogger.enableAutoLogging(true);
     
 
   }
@@ -42,32 +44,39 @@ public class Robot extends TimedRobot {
    * robot mode.
    */
   @Override
-  public void robotPeriodic() {
-    // Set the logger to log to the first flashdrive plugged in
-
-// Explicitly start the logger
-SignalLogger.start();
-
+  public void robotPeriodic() {}
+  @Override
+  public void teleopInit() {
+    m_toggle = true;
+    SignalLogger.start();
+    SignalLogger.enableAutoLogging(true);
 
   }
 
   /** The teleop periodic function is called every control packet in teleop. */
   @Override
   public void teleopPeriodic() {
+
+    voltageOut = Shuffleboard.getTab("Tuning").add("Voltage Out", voltageOut).getEntry().getDouble(voltageOut);
+    maxSpeed = Shuffleboard.getTab("Tuning").add("Max Speed", maxSpeed).getEntry().getDouble(maxSpeed);
     if (m_toggle) {
-      m_motor.setControl(new VoltageOut(12));
+      m_motor.setControl(new VoltageOut(voltageOut));
     } else {
       m_motor.stopMotor();
     }
     
-    if (Math.abs(m_motor.getVelocity().getValueAsDouble()) > 10.0) {
+    if (Math.abs(m_motor.getVelocity().getValueAsDouble()) > maxSpeed/60) {
       m_toggle = false;
+      SignalLogger.stop();
     }
 
+  }
+  private void log() {
     SignalLogger.writeDouble("position", m_motor.getPosition().getValueAsDouble());
     SignalLogger.writeDouble("veloicty", m_motor.getVelocity().getValueAsDouble());
     SignalLogger.writeDouble("acceleration", m_motor.getAcceleration().getValueAsDouble());
     SignalLogger.writeDouble("voltage", m_motor.getMotorVoltage().getValueAsDouble());
+
   }
 
   // @Override
